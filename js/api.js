9c3068d0d50b77783f72d000ff380deddddb96be
Kesley de Mello <kesley_ksl@hotmail.com>
2020-10-08 21:27:16 -0300

    Integrado cards

--- js/api.js
+++ js/api.js
@@ -1,11 +1,19 @@
 
-const url = 'https://api.themoviedb.org/3/genre/movie/list?api_key=af19825a1a0be0a03403c1e309e9e297&language=pt-BR'
+const baseUrl = 'https://api.themoviedb.org/3/'
 
+const baseImages = 'http://image.tmdb.org/t/p/w188_and_h282_bestv2/'
 
 async function getCategories(){
+    const url = baseUrl + 'genre/movie/list?api_key=af19825a1a0be0a03403c1e309e9e297&language=pt-BR'
     const response = await fetch(url);
     const genres = await response.json();
     return genres.genres
 }
  
+async function getNowPlaying(){
+    const url = baseUrl + 'movie/now_playing?api_key=af19825a1a0be0a03403c1e309e9e297&language=pt-BR&page=1'
 
+    const response = await fetch(url);
+    const data = await response.json();
+    return data.results
+}
--- js/main.js
+++ js/main.js
@@ -1,12 +1,36 @@
 const genresList = document.getElementById('genre-list');
-
-window.addEventListener('load', async ()=>{
+const main = document.querySelector('[data-main]');
+window.addEventListener('load', async () => {
+    console.log(main)
     const genres = await getCategories();
+    const nowPlaying = await getNowPlaying();
+    console.log(nowPlaying)
     createList(genres);
+    createCards(nowPlaying);
+
 })
 
 function createList(genres) {
     genres.forEach((genre) => {
         genresList.insertAdjacentHTML("beforeend", `<option value="${genre.id}">${genre.name}</option>`);
     })
+}
+
+function createCards(nowPlaying) {
+
+    nowPlaying.forEach((movie) => {
+        console.log(newCard(movie.title, movie.realease_date, movie.poster_path))
+        main.insertAdjacentHTML("beforeend", newCard(movie.title, movie.release_date, movie.poster_path))
+    })
+
+}
+
+function newCard(title, releaseDate, imagePath) {
+    let date = new Date(releaseDate).toLocaleDateString();
+    return `<div class="card"  style="background-image: url('${baseImages}${imagePath}');">
+                <div >
+                    <h5 class="card-title">${title}</h5>
+                    <p class="card-text">Lançamento${date}</p>
+                </div>
+            </div>`
 }
