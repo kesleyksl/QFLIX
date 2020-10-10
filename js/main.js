e40002037edcbfb74c34433c0cce1170fc33ef9c
Suyanne França <suyfranca@gmail.com>
2020-10-10 20:03:20 -0300

    Adiciona modal para o card clicado com as informações do filme

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const genresList = document.querySelector('[data-genre-list]');
 const main = document.querySelector('[data-main]');
 const searchButton = document.querySelector('[data-search-button]');
 const inputSearch = document.querySelector('[data-input-search]');
+const cardDiv = document.querySelector('.card');
 var movies = [];
 
 window.addEventListener('load', async () => {
@@ -13,8 +14,6 @@ window.addEventListener('load', async () => {
 
 })
 
-
-
 function createList(genres) {
     genres.forEach((genre) => {
         genresList.insertAdjacentHTML("beforeend", `<option value="${genre.id}">${genre.name}</option>`);
@@ -24,25 +23,58 @@ function createList(genres) {
 function createCards(nowPlaying) {
 
     nowPlaying.forEach((movie) => {
-        main.insertAdjacentHTML("beforeend", newCard(movie.title, movie.release_date, movie.poster_path, movie.overview))
+        main.insertAdjacentHTML("beforeend", newCard(movie.title, movie.id, movie.release_date, movie.poster_path, movie.overview))
     })
+}
+
+function newCard(title, id, releaseDate, imagePath) {
+    let date = new Date(releaseDate).toLocaleDateString('en-GB');
+    let image = imagePath == null ? 'https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_2-d537fb228cf3ded904ef09b136fe3fec72548ebc1fea3fbbd1ad9e36364db38b.svg' : `${baseImages}${imagePath}`
+    return `<div class="card" data-js="${id}"  style="background-image: url('${image}');">
+                <div class="details" data-js="${id}">
+                    <h5 class="card-title">${title}</h5>
+                    <p class="card-text">Lançamento - ${date}</p>
+
+                </div>
+            </div>`
+}
+
+async function cardDetails(movieId){
+    const details = await getMovieDetails(movieId);
+    return details;
+}
+
+function renderDetails(details){
+    let div = document.createElement('div');
+    div.setAttribute('class', 'cardDetails');
+    let date = new Date(details.release_date).toLocaleDateString('en-GB');
+
+    main.appendChild(div);
+    div.insertAdjacentHTML(
+        'beforeend',
+        `   
+        <div class="container-details">
+                <h1 class="cardDetails_title txt-purple"><center>${details.title}<center></h1>
+                <p><span>Gênero:</span> ${details.genres.map( genre => genre.name )}</p>
+                <p><span>Overview:</span> ${details.overview}</p>
+                <p><span>Lançamento:</span> ${date}</p>
+                <p><span>Duração:</span> ${details.runtime} minutos</p>
+                <p><span>Nota média:</span> ${details.vote_average}</p>
+            <div class="footer-details">
+                <center>
+                    <button type="button" onclick="hideDetails(event)" class="btn bg-purple txt-white" data-dismiss="modal">Fechar</button>
+                </center>
+                </div>
+        </div>
+`
+    )
 
 }
 
-function newCard(title, releaseDate, imagePath, overview) {
-    let date = new Date(releaseDate).toLocaleDateString();
-
-    let image = imagePath == null ? 'https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_2-d537fb228cf3ded904ef09b136fe3fec72548ebc1fea3fbbd1ad9e36364db38b.svg' : `${baseImages}${imagePath}`
-    console.log(image)
-    return `<div class="card"  style="background-image: url('${image}');">
-                <div class="details">
-                    <h5 class="card-title">${title}</h5>
-                    <p class="card-text">Lançamento${date}</p>
-                    <div class="description">
-                        ${overview}
-                    </div>
-                </div>
-            </div>`
+function hideDetails(){
+    if (main.lastElementChild.classList.contains('cardDetails')){
+        main.removeChild(main.lastElementChild);
+    }
 }
 
 async function filter(genreId, movieName) {
@@ -85,3 +117,14 @@ inputSearch.addEventListener('keyup', async (e) => {
         this.createCards(movies)
     }
 })
+
+main.addEventListener('click', async (e) => {
+    e.preventDefault();
+    hideDetails();
+    if (e.target.classList.contains('card') || e.target.classList.contains('details')){
+        console.log(e.target.dataset.js);
+        let details = await getMovieDetails(e.target.dataset.js);
+        renderDetails(details);
+    }
+
+});
